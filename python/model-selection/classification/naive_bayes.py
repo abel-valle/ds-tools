7471cdea650ff6b320530b1a864d5ebd435df76e
abel-valle <a.valle.chavez@gmail.com>
2022-06-08 23:20:09 -0500

    Add of model-selection for classification

---
+++ python/model-selection/classification/naive_bayes.py
@@ -0,0 +1,34 @@
+# Naive Bayes
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Importing the dataset
+df = pd.read_csv('Data.csv')
+x = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+
+# Feature Scaling
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+# Training the Naive Bayes model on the Training set
+classifier = GaussianNB()
+classifier.fit(x_train, y_train)
+
+# Making the Confusion Matrix
+y_pred = classifier.predict(x_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))

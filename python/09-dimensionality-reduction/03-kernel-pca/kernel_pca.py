df6c889a9d2a89212f027d57af9413f1b64a3d9e
abel-valle <a.valle.chavez@gmail.com>
2022-07-09 16:25:25 -0500

    Add of 09-03-kernel-pca

--- python/09-dimensionality-reduction/03-kernel-pca/kernel_pca.py
+++ python/09-dimensionality-reduction/03-kernel-pca/kernel_pca.py
@@ -5,42 +5,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import KernelPCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+from matplotlib.colors import ListedColormap
+
 # Importing the dataset
-dataset = pd.read_csv('Wine.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+df = pd.read_csv('Wine.csv')
+x = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
 
 # Applying Kernel PCA
-from sklearn.decomposition import KernelPCA
-kpca = KernelPCA(n_components = 2, kernel = 'rbf')
-X_train = kpca.fit_transform(X_train)
-X_test = kpca.transform(X_test)
+kpca = KernelPCA(n_components=2, kernel='rbf')
+x_train = kpca.fit_transform(x_train)
+x_test = kpca.transform(x_test)
 
 # Training the Logistic Regression model on the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(X_train, y_train)
+classifier = LogisticRegression(random_state=0)
+classifier.fit(x_train, y_train)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix, accuracy_score
-y_pred = classifier.predict(X_test)
+y_pred = classifier.predict(x_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
-accuracy_score(y_test, y_pred)
+print()
+print(accuracy_score(y_test, y_pred))
+print()
 
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
+# Visualizing the Training set results
+X_set, y_set = x_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
@@ -56,9 +59,8 @@ plt.ylabel('PC2')
 plt.legend()
 plt.show()
 
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
+# Visualizing the Test set results
+X_set, y_set = x_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
